1a0b5926a848be0198d9b7e03c5bd8c79bed45b2
Takayuki Hirasawa <istb16@gmail.com>
2022-04-03 11:06:03 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  let word = "鸞";
+  let size = 300;
+
+  const onFocus = (e: FocusEvent) => {
+    const it = e.currentTarget as HTMLInputElement;
+    it.select();
+  };
+</script>
+
+<main>
+  <div class="inputFields">
+    <input
+      type="text"
+      bind:value={word}
+      autofocus
+      on:focus={onFocus}
+      placeholder="文字を入力してください。"
+    />
+    <input type="range" bind:value={size} min="7" max="1000" />
+  </div>
+  <div class="word" style="font-size: {size}px">{word}</div>
+</main>
+
+<style>
+  .inputFields {
+    position: fixed;
+    inset: 0;
+    margin: auto;
+  }
+  .word {
+    margin-top: 2.5rem;
+  }
+</style>
